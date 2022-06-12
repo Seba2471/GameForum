1d5bee544d9e17ffea9c2f33dd6c8d5f4c89eace
Sebastian Siejak <siejak.sebastian@gmail.com>
2022-06-12 18:29:13 +0200

    Created ApplicationInstaller

---
+++ GameForum.Application/ApplicationInstallation.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+
+namespace GameForum.Application
+{
+    public static class ApplicationInstallation
+    {
+        public static IServiceCollection AddGameForumApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
