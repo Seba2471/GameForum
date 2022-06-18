d8cb9fcdb501df0fa240b60534c292ebf59fd273
Sebastian Siejak <siejak.sebastian@gmail.com>
2022-06-18 08:38:44 +0200

    Add BaseResponse class with ResponseStatus Enum

---
+++ GameForum.Application/Responses/BaseResponse.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation.Results;
+
+namespace GameForum.Application.Responses
+{
+    public class BaseResponse
+    {
+        public ResponseStatus Status { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        public List<string> ValidationErrors { get; set; }
+
+
+        public BaseResponse()
+        {
+            Success = true;
+            ValidationErrors = new List<string>();
+        }
+
+        public BaseResponse(string message = null)
+        {
+            Success = true;
+            Message = message;
+            ValidationErrors = new List<string>();
+        }
+
+        public BaseResponse(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+            ValidationErrors = new List<string>();
+        }
+
+        public BaseResponse(ValidationResult validationResult)
+        {
+            ValidationErrors = new List<string>();
+            Success = validationResult.Errors.Count < 0;
+            foreach (var item in validationResult.Errors)
+            {
+                ValidationErrors.Add(item.ErrorMessage);
+            }
+        }
+
+
+
+    }
+}
---
+++ GameForum.Application/Responses/ResponseStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace GameForum.Application.Responses
+{
+    public enum ResponseStatus
+    {
+        Success = 0,
+        NotFound = 1,
+        BadQuery = 2,
+        ValidationError = 3,
+    }
+}
