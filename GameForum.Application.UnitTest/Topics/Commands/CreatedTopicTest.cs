5f2a91d5a97e7f4c55b0971155b83d46ca09941a
Sebastian Siejak <siejak.sebastian@gmail.com>
2022-06-19 20:34:50 +0200

    Created topic test

---
+++ GameForum.Application.UnitTest/Topics/Commands/CreatedTopicTest.cs
@@ -0,0 +1,154 @@
+﻿using AutoMapper;
+using GameForum.Application.Contracts.Persistence;
+using GameForum.Application.Functions.Topics.Commands.CreateTopic;
+using GameForum.Application.Mapper;
+using GameForum.Application.UnitTest.Mocks;
+using Moq;
+using Shouldly;
+
+namespace GameForum.Application.UnitTest.Topics.Commands
+{
+    public class CreatedTopicTest
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ITopicRepository> _mockTopicRepository;
+
+        public CreatedTopicTest()
+        {
+            _mockTopicRepository = RepositoryMocks.GetTopicRepository();
+
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = configurationProvider.CreateMapper();
+        }
+
+        [Fact]
+        public async Task Handle_ValidTopic_AddedToTopicRepo()
+        {
+            var handler = new CreatedTopicCommandHandler(_mockTopicRepository.Object, _mapper);
+
+            var allPostsBeforeCount = (await _mockTopicRepository.Object.GetAllAsync()).Count();
+
+            var command = new CreatedTopicCommand()
+            {
+
+                Title = new string('*', 10),
+                Content = new string('*', 30),
+                DepartmentId = 1
+            };
+
+            var response = await handler.Handle(command, CancellationToken.None);
+
+            var allPosts = await _mockTopicRepository.Object.GetAllAsync();
+
+            response.Success.ShouldBe(true);
+            response.ValidationErrors.Count.ShouldBe(0);
+            allPosts.Count.ShouldBe(allPostsBeforeCount + 1);
+            response.TopicId.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task Handle_NotValidTopic_TooShortTitle_2Characters_NoAddedToTopicRepo()
+        {
+            var handler = new CreatedTopicCommandHandler(_mockTopicRepository.Object, _mapper);
+
+            var allPostsBeforeCount = (await _mockTopicRepository.Object.GetAllAsync()).Count();
+
+            var command = new CreatedTopicCommand()
+            {
+
+                Title = new string('*', 2),
+                Content = new string('*', 30),
+                DepartmentId = 1
+            };
+
+            var response = await handler.Handle(command, CancellationToken.None);
+
+            var allPosts = await _mockTopicRepository.Object.GetAllAsync();
+
+            response.Success.ShouldBe(false);
+            response.ValidationErrors.Count.ShouldBe(1);
+            allPosts.Count.ShouldBe(allPostsBeforeCount);
+            response.TopicId.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task Handle_NotValidTopic_TooLongTitle_31Characters_NoAddedToTopicRepo()
+        {
+            var handler = new CreatedTopicCommandHandler(_mockTopicRepository.Object, _mapper);
+
+            var allPostsBeforeCount = (await _mockTopicRepository.Object.GetAllAsync()).Count();
+
+            var command = new CreatedTopicCommand()
+            {
+
+                Title = new string('*', 31),
+                Content = new string('*', 30),
+                DepartmentId = 1
+            };
+
+            var response = await handler.Handle(command, CancellationToken.None);
+
+            var allPosts = await _mockTopicRepository.Object.GetAllAsync();
+
+            response.Success.ShouldBe(false);
+            response.ValidationErrors.Count.ShouldBe(1);
+            allPosts.Count.ShouldBe(allPostsBeforeCount);
+            response.TopicId.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task Handle_NotValidTopic_TooShortContent_2Characters_NoAddedToTopicRepo()
+        {
+            var handler = new CreatedTopicCommandHandler(_mockTopicRepository.Object, _mapper);
+
+            var allPostsBeforeCount = (await _mockTopicRepository.Object.GetAllAsync()).Count();
+
+            var command = new CreatedTopicCommand()
+            {
+
+                Title = new string('*', 25),
+                Content = new string('*', 2),
+                DepartmentId = 1
+            };
+
+            var response = await handler.Handle(command, CancellationToken.None);
+
+            var allPosts = await _mockTopicRepository.Object.GetAllAsync();
+
+            response.Success.ShouldBe(false);
+            response.ValidationErrors.Count.ShouldBe(1);
+            allPosts.Count.ShouldBe(allPostsBeforeCount);
+            response.TopicId.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task Handle_NotValidTopic_TooLongContent_501Characters_NoAddedToTopicRepo()
+        {
+            var handler = new CreatedTopicCommandHandler(_mockTopicRepository.Object, _mapper);
+
+            var allPostsBeforeCount = (await _mockTopicRepository.Object.GetAllAsync()).Count();
+
+            var command = new CreatedTopicCommand()
+            {
+
+                Title = new string('*', 25),
+                Content = new string('*', 501),
+                DepartmentId = 1
+            };
+
+            var response = await handler.Handle(command, CancellationToken.None);
+
+            var allPosts = await _mockTopicRepository.Object.GetAllAsync();
+
+            response.Success.ShouldBe(false);
+            response.ValidationErrors.Count.ShouldBe(1);
+            allPosts.Count.ShouldBe(allPostsBeforeCount);
+            response.TopicId.ShouldBeNull();
+        }
+
+    }
+}
