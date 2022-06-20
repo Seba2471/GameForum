8735152c7aff555fc71f61b3ce6b35920a43ea69
Sebastian Siejak <siejak.sebastian@gmail.com>
2022-06-20 06:38:39 +0200

    Created game forum db context

---
+++ GameForum.Persistence.EF/GameForumContext.cs
@@ -0,0 +1,37 @@
+﻿using GameForum.Domain.Common;
+using GameForum.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameForum.Persistence.EF
+{
+    public class GameForumContext : DbContext
+    {
+        public GameForumContext(DbContextOptions<GameForumContext> options) : base(options)
+        { }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly
+                (typeof(GameForumContext).Assembly);
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
