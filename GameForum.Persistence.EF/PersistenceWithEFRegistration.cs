b9f685c00a625d1aef450db89116868887ad3904
Sebastian Siejak <siejak.sebastian@gmail.com>
2022-06-20 06:42:47 +0200

    Created persistence with entity framework registration

---
+++ GameForum.Persistence.EF/PersistenceWithEFRegistration.cs
@@ -0,0 +1,24 @@
+﻿using GameForum.Application.Contracts.Persistence;
+using GameForum.Persistence.EF.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GameForum.Persistence.EF
+{
+    public static class PersistenceWithEFRegistration
+    {
+        public static IServiceCollection AddGameForumPersistenceEFServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<GameForumContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("GameForumConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<ITopicRepository, TopicRepository>();
+            services.AddScoped<IPostRepository, PostRepository>();
+
+            return services;
+        }
+    }
+}
