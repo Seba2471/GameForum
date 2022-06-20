2068c13c1a6ac44e6954938e339a68f2183b5ccf
Sebastian Siejak <siejak.sebastian@gmail.com>
2022-06-20 06:40:51 +0200

    Created Repositories

---
+++ GameForum.Persistence.EF/Repositories/PostRepository.cs
@@ -0,0 +1,12 @@
+﻿using GameForum.Application.Contracts.Persistence;
+using GameForum.Domain.Entities;
+
+namespace GameForum.Persistence.EF.Repositories
+{
+    public class PostRepository : BaseRepository<Post>, IPostRepository
+    {
+        public PostRepository(GameForumContext dbContext) : base(dbContext)
+        { }
+
+    }
+}
---
+++ GameForum.Persistence.EF/Repositories/TopicRepository.cs
@@ -0,0 +1,17 @@
+﻿using GameForum.Application.Contracts.Persistence;
+using GameForum.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameForum.Persistence.EF.Repositories
+{
+    public class TopicRepository : BaseRepository<Topic>, ITopicRepository
+    {
+        public TopicRepository(GameForumContext dbContext) : base(dbContext)
+        { }
+
+        public async Task<Topic?> GetTopicByIdWithPostsList(int topicId)
+        {
+            return await _dbContext.Topics.Include(t => t.Posts).FirstOrDefaultAsync(t => t.TopicId == topicId);
+        }
+    }
+}
