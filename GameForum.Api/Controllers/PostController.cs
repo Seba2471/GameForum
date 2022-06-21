410ea136407d8fda27fb0eb613b529a1dbf463d4
Sebastian Siejak <siejak.sebastian@gmail.com>
2022-06-21 19:53:22 +0200

    Created Post and Topic Controller with methods Created

---
+++ GameForum.Api/Controllers/PostController.cs
@@ -0,0 +1,26 @@
+﻿using GameForum.Application.Functions.Posts.Commands.CreatePost;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameForum.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PostController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost(Name = "AddPost")]
+        public async Task<ActionResult<int>> Create([FromBody] CreatedPostCommand createdPostCommand)
+        {
+            var result = await _mediator.Send(createdPostCommand);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ GameForum.Api/Controllers/TopicController.cs
@@ -0,0 +1,26 @@
+﻿using GameForum.Application.Functions.Topics.Commands.CreateTopic;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameForum.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TopicController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TopicController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost(Name = "AddTopic")]
+        public async Task<ActionResult<int>> Create([FromBody] CreatedTopicCommand createdTopicCommand)
+        {
+            var result = await _mediator.Send(createdTopicCommand);
+
+            return Ok(result);
+        }
+    }
+}
