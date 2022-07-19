74e013cc63226a954cbc2289afd0716aca8ffab9
Sebastian Siejak <siejak.sebastian@gmail.com>
2022-07-19 18:04:39 +0200

    Added configure identity option. Setting password policy to false and require unique email adress on true

--- GameForum.Persistence.EF/PersistenceWithEFRegistration.cs
+++ GameForum.Persistence.EF/PersistenceWithEFRegistration.cs
@@ -27,6 +27,30 @@ namespace GameForum.Persistence.EF
                 .AddEntityFrameworkStores<GameForumContext>()
                 .AddDefaultTokenProviders();
 
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // SignIn settings
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+
+                // User settings
+                options.User.RequireUniqueEmail = true;
+            });
+
+
+
             services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
 
             services.AddScoped<ITopicRepository, TopicRepository>();
