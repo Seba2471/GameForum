ff81a722b316c76e527cb863274a3963727f4081
Sebastian Siejak <siejak.sebastian@gmail.com>
2022-07-26 07:25:28 +0200

    Update user register to add new user to User role

--- GameForum.Application/Functions/Users/Commands/RegisterUser/RegisterUserCommandHandler.cs
+++ GameForum.Application/Functions/Users/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -10,10 +10,12 @@ namespace GameForum.Application.Functions.Users.Commands.RegisterUser
     public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, OneOf<Success, NotValidateResponse, IdentityErrorResponse, Error>>
     {
         private UserManager<ApplicationUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
 
-        public RegisterUserCommandHandler(UserManager<ApplicationUser> userManager)
+        public RegisterUserCommandHandler(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public async Task<OneOf<Success, NotValidateResponse, IdentityErrorResponse, Error>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
@@ -32,6 +34,14 @@ namespace GameForum.Application.Functions.Users.Commands.RegisterUser
 
             if (result.Succeeded)
             {
+                var role = _roleManager.FindByNameAsync("User").Result;
+
+                if (role != null)
+                {
+                    await _userManager.AddToRoleAsync(user, role.Name);
+                }
+
+
                 return new Success();
             }
 
